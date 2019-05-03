eb96d5d09166c6bf7c52496697fc6b55d7f737ea
Devon T. Wesley <dwesley89@gmail.com>
2019-05-02 17:27:30 -0700

    fix: removed chain data

--- startService.js
+++ startService.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 
 const SERVICE_NAME = './services/mgeth';
 const MGETH_DATADIR = `--datadir=${process.cwd()}/services/ETC`;
-const SERVICE_FLAGS = ['--classic', '--rpcport=8000', '--rpc', MGETH_DATADIR]
+const SERVICE_FLAGS = ['--rpccorsdomain=*', '--classic', '--rpcport=8545', '--rpc', MGETH_DATADIR]
 
 function startService(serviceName, args) {
   spawn(serviceName, args, { stdio: 'inherit' });
